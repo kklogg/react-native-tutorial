08062c59f23663e8f15d55305d8ab4027829ce16
wlthomson <will.tho@outlook.com>
2018-11-20 02:43:42 +1300

    Add ButtonBasics component

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
-const pizza = '\uD83C\uDF55';
-
-export default class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!');
   }
 
   render() {
     return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && pizza).join(' ')}
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this._onPressButton} title='Press Me'/>
+        </View>
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
